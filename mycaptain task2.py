b0259aa2c69185da55b88a338cce13010f3176b1
anirithaT <78675927+anirithaT@users.noreply.github.com>
2021-02-07 23:08:52 +0530

    Update mycaptain task2.py

--- mycaptain task2.py
+++ mycaptain task2.py
@@ -1,4 +1,4 @@
-filename= input("Input the Filename")
-extension= input("The extension of the file is :")
-print("Input the filename:",filename)
-print("The extension of the file is: ", extension)
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+if f_extns[-1]== "py":
+    print ("The  extension of the file is : " + "python")
